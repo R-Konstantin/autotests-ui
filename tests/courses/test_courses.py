c0997f88bce55e5566a8c061295d9ceac847f129
R-Konstantin <k.rodionov777@gmail.com>
2026-04-01 20:03:11 +0300

    Наращивание тестовой базы и рефакторинг

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -108,6 +108,57 @@ class TestCourses:
             estimated_time='2 weeks'
         )
 
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(True)
+        create_course_page.create_course_form.fill(
+            index=0,
+            title='Playwright',
+            description='Playwright',
+            estimated_time='2 weeks',
+            max_score="100",
+            min_score="10"
+        )
+
+        create_course_page.toolbar_view.click_create_course_button()
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title='Playwright',
+            max_score="100",
+            min_score="10",
+            estimated_time='2 weeks'
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(
+            index=0,
+            title='Edit title',
+            description='Edit description',
+            estimated_time='5 weeks',
+            max_score="111",
+            min_score="22"
+        )
+
+        create_course_page.toolbar_view.click_create_course_button()
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title='Edit title',
+            max_score="111",
+            min_score="22",
+            estimated_time='5 weeks'
+        )
+
+
+
+
+
+
+
+
 
 
 
