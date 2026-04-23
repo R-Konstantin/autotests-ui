7212ab4bad030e79f4141f1e6ed81136363fc2d3
R-Konstantin <k.rodionov777@gmail.com>
2026-04-23 19:27:46 +0300

    Моки и оптимизация UI автотестов

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,6 +2,8 @@ from typing import Any, Generator
 import allure
 from playwright.sync_api import Playwright, Page
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
     playwright: Playwright,
@@ -10,9 +12,15 @@ def initialize_playwright_page(
     storage_state: str | None = None
 ) -> Generator[Page, Any, None]:
     browser = playwright[browser_type].launch(headless=settings.headless)
-    context = browser.new_context(base_url=settings.get_base_url(), storage_state=storage_state, record_video_dir=settings.videos_dir)
+    context = browser.new_context(
+        base_url=settings.get_base_url(),
+        storage_state=storage_state,
+        record_video_dir=settings.videos_dir
+    )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
