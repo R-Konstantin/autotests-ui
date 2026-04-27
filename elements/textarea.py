68de11d45b8e6c93f03a6c2fbff10af1cdc4debd
R-Konstantin <k.rodionov777@gmail.com>
2026-04-27 22:46:51 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/textarea.py
+++ elements/textarea.py
@@ -1,7 +1,7 @@
 from elements.base_element import BaseElement
 from playwright.sync_api import expect, Locator
 import allure
-
+from ui_coverage_tool import ActionType
 from tools.logger import get_logger
 
 logger = get_logger("TEXTAREA")
@@ -14,6 +14,9 @@ class Textarea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f"{super().get_locator(nth, **kwargs)}//textarea"
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
 
@@ -22,6 +25,8 @@ class Textarea(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
 
@@ -29,3 +34,5 @@ class Textarea(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
